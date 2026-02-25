0e37c3bc48ea1f3e5e6c701a54785b29dd205bc5
Palii Dorin <palii.dorin@yandex.ru>
2026-02-25 14:16:05 +0200

    change orders in offers

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -24,7 +24,7 @@ const mainNavItems: NavItem[] = [
         icon: LayoutGrid,
     },
     {
-        title: 'Orders',
+        title: 'Offers',
         href: orders(),
         icon: ListOrdered,
     },
